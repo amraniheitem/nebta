9f384e53c16ed2147c0c4094e4a79297d78b1544
amraniheitem <165966339+amraniheitem@users.noreply.github.com>
2024-04-07 03:53:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-const express = require('express')
-const Connectdb = require('./database/Connectdb')
-const bodyParser = require('body-parser')
+const express = require('express');
+const Connectdb = require('./database/Connectdb');
+const bodyParser = require('body-parser');
 const httpStatusText = require('./utils/httpStatusText');
-const cors = require('cors')
+const cors = require('cors');
 const Role = require("./middleware/role-auth");
-require('dotenv').config()
+require('dotenv').config();
 
-const port = process.env.PORT || 8000
-const app = express()
+const port = process.env.PORT || 8000;
+const app = express();
 
+const authRoutes = require('./router/authRoutes'); 
 
-
-const authRoutes = require('./router/authRoutes');
-const productRouter = require('./router/productRouter')
-const articleRouter = require('./router/home/article.Router')
-const CodePromoRouter = require('./router/home/CodePromo.Router')
-const NewsRouter = require('./router/home/news.Router')
-const offrePromotionRouter = require('./router/home/OffrePromotion.Router')
-const ConseilleRouter = require("./router/home/Conseille.Router")
+const productRouter = require('./router/productRouter');
+const articleRouter = require('./router/home/article.Router');
+const CodePromoRouter = require('./router/home/CodePromo.Router');
+const NewsRouter = require('./router/home/news.Router');
+const offrePromotionRouter = require('./router/home/OffrePromotion.Router');
+const ConseilleRouter = require("./router/home/Conseille.Router");
 const authJwt = require('./middleware/jwt');
 
-Connectdb()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(authJwt);
+Connectdb();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api/product',Role("chercheur"), productRouter);
-app.use('/api/article',articleRouter)
-app.use('/api/CodePromo',CodePromoRouter)
-app.use('/api/news',NewsRouter)
-app.use('/api/offredePromotion',offrePromotionRouter);
-app.use('/api/Conseille',ConseilleRouter);
-app.use('/api/auth', authRoutes);
+app.use('/api/product', productRouter);
+app.use('/api/article', articleRouter);
+app.use('/api/CodePromo', CodePromoRouter);
+app.use('/api/news', NewsRouter);
+app.use('/api/offredePromotion', offrePromotionRouter);
+app.use('/api/Conseille', ConseilleRouter);
+app.use('/api/auth', authRoutes); 
 
+app.use(authJwt); 
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({status: httpStatusText.ERROR, msg: "cannot found data"});
+});
 
+app.use((error, req, res, next) => {
+    res.status(error.statusCode || 500).json({status: httpStatusText.ERROR, msg: error.message});
+});
 
-app.all('*',(req,res,next)=>{
-    res.status(404).json({status : httpStatusText.ERROR , msg : "cannot found data"});
-})
-
-app.use('*',(error,req,res,next)=>{
-    res.status(error.statusCode || 500).json({status : httpStatusText.ERROR , msg : error.message});
-})
-
-app.listen(port,()=>{
-    console.log(`Listening to ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Listening to ${port}`);
+});
