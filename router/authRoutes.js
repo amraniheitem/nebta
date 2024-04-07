1438bfc66606c3f6ed83094b294b176ea6578251
amraniheitem <165966339+amraniheitem@users.noreply.github.com>
2024-04-07 04:23:53 +0100

    Update authRoutes.js

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../Controller/authController');
+require('dotenv').config();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
